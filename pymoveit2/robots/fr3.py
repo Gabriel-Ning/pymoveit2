7756279fbdc608db1d49ad4eeb3b06fda75d1a7c
Gabriel-Ning <guomning@gmail.com>
2025-03-08 07:20:30 +0000

    add franka fr3 robot

---
+++ pymoveit2/robots/fr3.py
@@ -0,0 +1,43 @@
+from typing import List
+
+MOVE_GROUP_ARM: str = "fr3_arm"
+MOVE_GROUP_GRIPPER: str = "gripper"
+
+OPEN_GRIPPER_JOINT_POSITIONS: List[float] = [0.04, 0.04]
+CLOSED_GRIPPER_JOINT_POSITIONS: List[float] = [0.0, 0.0]
+
+
+def joint_names(prefix: str = "fr3_") -> List[str]:
+    return [
+        prefix + "joint1",
+        prefix + "joint2",
+        prefix + "joint3",
+        prefix + "joint4",
+        prefix + "joint5",
+        prefix + "joint6",
+        prefix + "joint7",
+    ]
+
+
+def base_link_name(prefix: str = "fr3_") -> str:
+    return prefix + "link0"
+
+
+def end_effector_name(prefix: str = "fr3_") -> str:
+    return prefix + "hand_tcp"
+
+
+def gripper_joint_names(prefix: str = "fr3_") -> List[str]:
+    return [
+        prefix + "finger_joint1",
+        prefix + "finger_joint2",
+    ]
+
+
+def end_effector_with_gripper_name(prefix: str = "fr3_") -> str:
+    return prefix + "hand_tcp"
+    
+
+
+def end_effector_without_gripper_name(prefix: str = "fr3_") -> str:
+    return prefix + "link8"
